ff8d3a0db732fd66551c546554aef49b019f51f9
lothar0824 <54477119+lothar0824@users.noreply.github.com>
2022-10-23 13:35:35 -0400

    backend_commit_taskadd_finish_delete

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const multer = require('multer')
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const User = require('./model/user')
+const UserTask = require('./model/tasks')
 const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken') 
+const jwt = require('jsonwebtoken')
 
 const JWT_SECRET = ';oidhjf;lkdahjsj li;furqw[oitvufenoioungfoisaud[oifusao;+'
-var authed = false
 
-//multer file storage and stuff
+/*multer file storage and stuff
 var Storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, "./userFiles");
@@ -26,7 +26,7 @@ var upload = multer({
   storage: Storage,
 }).single("file"); //Field name and max count
 
-//end multer stuff here
+end multer stuff here*/
 
 app.use(express.static(pathe.join(__dirname, 'public')));
 app.use(bodyParser.json())
@@ -40,18 +40,96 @@ mongoose.connect('mongodb://localhost:27017/AuthAppDB',{
   useUnifiedTopology:true
 });
 
-
 app.set("view engine", "ejs")
+
+
 app.get('/', async (req,res) => {
-  if(!authed)
-    {res.render("login")}
-  else
-  {
-    res.render("Dashboard", {Information:UserInfo})
-  }
+  res.render("Login");
 })
 
-var UserInfo = new Object();
+
+const authenticator = async function (req, res, next){
+  try {
+    const { token } = req.body
+    console.log("token : " + token)
+
+    if(!token)
+    {
+      return res.json({status:'not Authenticated, token was not recieved'})
+    }
+    else
+    {
+      const user = jwt.verify(token,JWT_SECRET)
+      console.log('JWT decoded: ', user)
+      if(!user)
+      {
+        return res.json({status:'not Authenticated'})
+      }
+      req.user = user
+      next()
+    }
+    
+  } catch (error) {
+    return res.json({status:'not Authenticated'})
+  }
+  
+}
+
+app.post('/DisplayInfo',authenticator, async (req, res) => {
+  res.json({status:'ok', data: req.user})
+})
+
+app.post("/findalltasks", authenticator, async (req, res) => {
+
+  const response = await UserTask.find({
+    email: req.user.email
+  })
+  return res.json({status:'ok',data:response})
+})
+
+app.post("/addtask", authenticator, async (req, res) => {
+
+  email = req.user.email
+  console.log(req.body.taskn)
+  const taskName = req.body.taskn
+  const taskdesc = req.body.taskd
+  
+    newTask = new UserTask({
+      task: taskName,
+      email: email,
+      taskdesc: taskdesc
+    })
+    newTask.save(function (err, results) {
+      if(err) 
+      {
+        console.log(err)
+        return res.json({status:'ok',data:"not inserted"})
+      }
+      else
+      {
+        console.log(results._id);
+        return res.json({status:'ok',data:"inserted"})
+      }
+    }); 
+})
+
+app.post("/updateTask", authenticator, async (req, res) => {
+  const newstatus = req.body.updatestatus
+  const taskId = req.body.taskid
+  
+  if(newstatus == 'delete')
+  {
+    const updateStatus = await UserTask.findByIdAndDelete({_id:taskId})
+    return res.json({status:'ok', data:'task deleted'})
+  }
+  const updateStatus = await UserTask.findByIdAndUpdate({_id:taskId}, {status:newstatus})
+  return res.json({status:'ok',data:'status updated'})
+})
+
+app.get('/Dashboard', async (req, res) => {
+  res.render('Dashboard');
+})
+
 
 app.post("/signin", async (req,res) => {
 
@@ -64,11 +142,11 @@ app.post("/signin", async (req,res) => {
     //email password combination successful
     const token = jwt.sign({
       id: user._id,
-      username: user.username
-    }, JWT_SECRET)
-    authed = true;
-    UserInfo.name = user.username;
-    UserInfo.id = user._id;
+      username: user.username,
+      email: user.email,
+      phonenumber: user.phonenumber,
+      bday: user.bday
+    }, JWT_SECRET, {expiresIn: '24h'})
     return res.json({status:'ok', data: token})
   }
     return res.json({status:'error', error: 'Invalid password'})
@@ -133,9 +211,10 @@ app.post("/signup",async (req,res)=>{
 
 })
 
-app.get('/logout', (req,res) => {
-  authed = false;
-  res.redirect('/');
+
+
+app.get('*', function(req, res){
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
